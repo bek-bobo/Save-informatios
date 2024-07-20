1ecafdf9e505ecf3fb1ce944123fe9c7e1503d4c
Behzod <alzosidi@gmail.com>
2024-07-20 21:24:09 +0500

    project configure

---
+++ src/main/java/com/example/saveinformation/SaveInformationApplication.java
@@ -0,0 +1,13 @@
+package com.example.saveinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveInformationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveInformationApplication.class, args);
+    }
+
+}
